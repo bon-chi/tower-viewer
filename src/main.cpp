360a5acacd2a185a042c285d04a9cbab01129237
Koji Ota <afterjnih@gmail.com>
2019-05-02 03:50:17 +0900

    format

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <poppler-document.h>
 #include <poppler-page-renderer.h>
 
-class MyArea : public Gtk::DrawingArea
-{
+using namespace std;
+
+class MyArea : public Gtk::DrawingArea {
  public:
   MyArea();
   virtual ~MyArea();
@@ -12,16 +13,11 @@ class MyArea : public Gtk::DrawingArea
   bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
 };
 
-MyArea::MyArea()
-{
-}
+MyArea::MyArea() {}
 
-MyArea::~MyArea()
-{
-}
+MyArea::~MyArea() {}
 
-bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
-{
+bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
   Gtk::Allocation allocation = get_allocation();
   const int width = allocation.get_width();
   const int height = allocation.get_height();
@@ -29,15 +25,17 @@ bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
   auto page = document->create_page(0);
   poppler::page_renderer renderer;
   poppler::image img = renderer.render_page(page);
-  auto *bar = (guint8 *)(unsigned char*)(img.data());
-  auto m_image = Gdk::Pixbuf::create_from_data(bar, Gdk::Colorspace::COLORSPACE_RGB, true, 8, img.width(), img.height(), img.bytes_per_row());
-  Gdk::Cairo::set_source_pixbuf(cr, m_image, (width - m_image->get_width())/2, (height - m_image->get_height())/2);
+  auto* bar = (guint8*)(unsigned char*)(img.data());
+  auto m_image = Gdk::Pixbuf::create_from_data(bar, Gdk::Colorspace::COLORSPACE_RGB, true, 8,
+                                               img.width(), img.height(), img.bytes_per_row());
+  Gdk::Cairo::set_source_pixbuf(cr, m_image, (width - m_image->get_width()) / 2,
+                                (height - m_image->get_height()) / 2);
   cr->paint();
 
   return true;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   auto app = Gtk::Application::create(argc, argv, "com.github.bon-chi.tower-viewer");
 
   Gtk::Window window;
